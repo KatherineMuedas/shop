495efae7182e3305de9e35ffe59651460c5918fc
katherine <muedasnka@gmail.com>
2015-05-27 21:10:23 -0600

    adds a seccion counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+  end
   def index
+    @count = increment_count
     @products = Product.order(:title)
   end
 end
